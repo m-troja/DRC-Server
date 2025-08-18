64e9bcd5d01aa7b7ce1407e3498c14c11ec34280
Łukasz Michalak <lupinummail@gmail.com>
2025-08-18 23:10:00 +0200

    allowing react for connection

--- src/main/java/com/drc/server/websocket/StompConfiguration.java
+++ src/main/java/com/drc/server/websocket/StompConfiguration.java
@@ -3,16 +3,9 @@ package com.drc.server.websocket;
 import com.drc.server.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -44,8 +37,7 @@ public class StompConfiguration implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
         registry.addEndpoint("/game")
-                .setAllowedOrigins("http://localhost:3000")
-                .setAllowedOrigins("http://localhost")
+                .setAllowedOrigins("http://localhost", "http://localhost:3000")
                 .setHandshakeHandler(new UserHandshakeHandler())
                 .addInterceptors(handshakeInterceptor)
                 .withSockJS();
