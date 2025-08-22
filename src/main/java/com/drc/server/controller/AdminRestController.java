c5e0bcfdf29977e76f33b926239aae26cf0a4b01
Michal Trojanowski <mtroja98@gmail.com>
2025-08-22 15:48:21 +0200

    Implement minimum players qty with config file

--- src/main/java/com/drc/server/controller/AdminRestController.java
+++ src/main/java/com/drc/server/controller/AdminRestController.java
@@ -2,8 +2,10 @@ package com.drc.server.controller;
 
 import com.drc.server.entity.Game;
 import com.drc.server.service.GameService;
+import com.drc.server.websocket.WebSocketSessionRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,36 +20,40 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminRestController {
 
     private final GameService gameService;
+    private final WebSocketSessionRegistry webSocketSessionRegistry;
 
     private static final String START_GAME = "START_GAME";
     private static final String NEXT_QUESTION = "NEXT_QUESTION";
-    private static final Integer minimumPlayerQty = 1;
     private static final String NOT_ENOUGH_PLAYERS = "Error: not enough players connected. Required: ";
     private static final String NO_PLAYERS = "Error: no users connected";
     private static final String SC_OK = "Game started {}";
 
+    @Value("${minimum.players.qty}")
+    private Integer MINIMUM_PLAYERS_QTY ;
+
     @GetMapping("/cmd")
     public ResponseEntity<String> startGame(@RequestParam("cmd") String cmd) {
 
+        int playersConnected = webSocketSessionRegistry.getAllSessions().size() ;
         log.debug("cmd: {}", cmd);
 
         Game game = new Game();
 
-        if (cmd.equals(START_GAME))  {
-            game = gameService.startNewGame();
-            if( game == null) {
-                log.debug("Error: game is null! Sending SC_412");
-                return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED)
-                        .body(NO_PLAYERS);
+        if (cmd.equals(START_GAME) ) {
+            if (playersConnected >= MINIMUM_PLAYERS_QTY) {
+                game = gameService.startNewGame();
+                if (game == null) {
+                    log.debug("Server error! Please contact orzeu");
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                            .body(NO_PLAYERS);
+                }
+                log.debug("Game started correctly {}", game);
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body("GameId: " + game.getId());
             }
-            else if (game.getUsers().size() < minimumPlayerQty) {
-                log.debug("Error: not enough players connected! Sending SC_412 for {}", game);
-                return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED)
-                        .body(NOT_ENOUGH_PLAYERS + minimumPlayerQty);
+            else {
+                return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Minimum players qty not reached. Needed: " + MINIMUM_PLAYERS_QTY + ", actual: " + playersConnected);
             }
-            log.debug(SC_OK, game);
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body("GameId: " + game.getId());
         }
         else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Command not supported");
