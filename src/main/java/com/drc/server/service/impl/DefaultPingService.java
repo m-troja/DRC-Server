219020ddfd9ca21783f608829d600ca3bc5eec9e
Michal Trojanowski <mtroja98@gmail.com>
2025-08-24 16:15:40 +0200

    Improved exception handling

--- src/main/java/com/drc/server/service/impl/DefaultPingService.java
+++ src/main/java/com/drc/server/service/impl/DefaultPingService.java
@@ -1,6 +1,7 @@
 package com.drc.server.service.impl;
 
 import com.drc.server.entity.PingMessage;
+import com.drc.server.entity.User;
 import com.drc.server.service.PingService;
 import com.drc.server.websocket.WebSocketSessionRegistry;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -19,11 +22,17 @@ public class DefaultPingService implements PingService {
 
     @Scheduled(fixedRate = 10000) // every 10k ms = every 10 s
     public void sendPing() {
-        if ( !webSocketSessionRegistry.getAllSessions().isEmpty())
+        Map<String, User> sessions = webSocketSessionRegistry.getAllSessions();
+        PingMessage pingMessage = new PingMessage(System.currentTimeMillis());
+
+        if ( !sessions.isEmpty())
         {
-            PingMessage pingMessage = new PingMessage(System.currentTimeMillis());
+            for (String session : sessions.keySet()) {
+                User user = sessions.get(session);
+                log.debug("Sent ping to {}: {}" , user.getName(), pingMessage.ping());
+            }
             messagingTemplate.convertAndSend("/client/ping", pingMessage);
-            log.debug("Sent ping to clients: {}" , pingMessage);
+
         }
     }
 
